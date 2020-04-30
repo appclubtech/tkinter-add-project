5ada806a8a3ebbe7148aaca3a56b7484314a7718
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-05-01 01:00:51 +0500

    update comment and readability

--- main/add_num_gui.py
+++ main/add_num_gui.py
@@ -18,49 +18,46 @@ class AddNumApp:
         self.style.theme_use('default')
 
         # create label widget which shows the result
-        self.result = ttk.Label(master, text="0", justify=CENTER, background="#FFFFFF")
-        self.result.grid(row=1, column=0, padx=10, pady=22, columnspan=2)
+        self.result_view = ttk.Label(master, text="0", background="#FFFFFF")
+        self.result_view.grid(row=0, column=0, padx=10, pady=22, columnspan=2)
 
         # create entry widget where user inputs the first value
         self.first_value = ttk.Entry(master, width=10)
-        self.first_value.grid(row=2, column=0, padx=10, pady=10)
+        self.first_value.grid(row=1, column=0, padx=10, pady=10)
 
         # create entry widget where user inputs the second value
         self.second_value = ttk.Entry(master, width=10)
-        self.second_value.grid(row=2, column=1, padx=10, pady=10)
+        self.second_value.grid(row=1, column=1, padx=10, pady=10)
 
         # create button widget which adds first and second value
-        self.add_button = ttk.Button(master, width=8, text="Add", command=self.add)
-        self.add_button.grid(row=3, column=0, padx=10, pady=5)
+        ttk.Button(master, width=8, text="Add", command=self.add).grid(row=2, column=0, padx=10, pady=5)
 
         # create button widget which clears input
-        self.add_button = ttk.Button(master, width=8, text="Clear", command=self.clear)
-        self.add_button.grid(row=3, column=1, padx=10, pady=12)
+        ttk.Button(master, width=8, text="Clear", command=self.clear).grid(row=2, column=1, padx=10, pady=12)
 
     def add(self):
         try:
-            # get first value
+            # get value from self.first_value
             first_value = self.first_value.get()
-            # get second value
+            # get value from self.second_value
             second_value = self.second_value.get()
 
-            # validate first value is not empty
+            # validate self.first_value is not empty
             if first_value == "":
                 messagebox.showerror("Value Error", "Enter first value")
                 return
 
-            # validate second value is not empty
+            # validate self.second_value is not empty
             if second_value == "":
                 messagebox.showerror("Value Error", "Enter second value")
                 return
 
-            # getting addition result by adding first and second value
+            # add both values
             result = self.add_num(float(first_value), float(second_value))
 
-            # setting result value to label widget
-            self.result.configure(text='{} + {} = {}'.format(str(first_value),
-                                                             str(second_value),
-                                                             str(result)))
+            # set the calculated value to self.result_view
+            self.result_view.configure(text='{} + {} = {}'.format(str(first_value), str(second_value), str(result)))
+
         except ValueError:
             # displaying error message box
             messagebox.showerror("Invalid input", "Invalid input detected. Numbers are only supported")
@@ -76,7 +73,7 @@ class AddNumApp:
     def clear(self):
         self.first_value.delete(0, 'end')
         self.second_value.delete(0, 'end')
-        self.result.configure(text='0')
+        self.result_view.configure(text='0')
 
 
 def main():
